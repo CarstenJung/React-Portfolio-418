f8d29c4ef6757625181006f4a62fa1b57597326b
Carsten Jung <39696700+CarstenJung@users.noreply.github.com>
2023-07-01 12:31:54 +0200

    lt

--- Portfolio/src/components/DataSlider.jsx
+++ Portfolio/src/components/DataSlider.jsx
@@ -1,42 +1,68 @@
-// Code a 2 row layout
-// Fill first row with title, description, link, url etc.
-// Fill second row with images
-// Make the first row sticky when comes to the top of the viewport
-// Feet it with the Education and Portfolio Data accordingl to the image
-// Use Gsap or observer for this
-// When the Component ends scroll to the next component. For that remove sticky.
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const DataSlider = ({ currentItem, setCurrentItem, btnText, dataItem }) => {
+  const dataSliderWrapperRef = useRef(null);
+  const dataSliderLeftRef = useRef(null);
+  const dataSliderRightRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const dataSliderWrapper = dataSliderWrapperRef.current;
+    const dataSliderLeft = dataSliderLeftRef.current;
+    const dataSliderRight = dataSliderRightRef.current;
+
+    ScrollTrigger.create({
+      trigger: dataSliderWrapper,
+      start: "top top",
+      end: "bottom top",
+      pin: dataSliderLeft,
+      pinSpacing: false,
+      markers: true, // For debugging purposes
+    });
+
+    ScrollTrigger.create({
+      trigger: dataSliderWrapper,
+      start: "top top",
+      end: () => `+=${dataSliderRight.offsetHeight}`,
+      pin: dataSliderRight,
+      pinSpacing: false,
+      markers: true, // For debugging purposes
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
+
   return (
     <div className="dataSlider section">
-      <div className="dataSliderWrapper">
-        <div className="dataSliderLeft">
+      <div className="dataSliderWrapper" ref={dataSliderWrapperRef}>
+        <div className="dataSliderLeft" ref={dataSliderLeftRef}>
           <div className="dataTextContainer">
             <h1>{currentItem.title}</h1>
             <h3>{currentItem.subtitle}</h3>
             <p>{currentItem.description}</p>
             <p>{currentItem.subdescription}</p>
-            <a href={currentItem.link} target="_blank">
+            <a href={currentItem.link} target="_blank" rel="noopener noreferrer">
               {btnText}
             </a>
           </div>
         </div>
-        <div className="dataSliderRight" style={{height: dataItem.length + '00vh'}}>
-          {dataItem.map((item) => {
-            return (
-              <img
-                src={item.image}
-                alt={item.title}
-                className={
-                    // TODO CHECK HOW TO USE THIS TO COUPLE WITH CURRENT ITEM
-                  item.id === currentItem.id
-                    ? "dataSliderImage active"
-                    : "dataSliderImage"
-                }
-                key={item.id}
-              ></img>
-            );
-          })}
+        <div className="dataSliderRight" ref={dataSliderRightRef}>
+          {dataItem.map((item) => (
+            <div
+              key={item.id}
+              className={
+                item.id === currentItem.id ? "slideImgWrapper active" : "slideImgWrapper"
+              }
+            >
+              <img src={item.image} alt={item.title} className="dataSliderImage" />
+            </div>
+          ))}
         </div>
       </div>
     </div>
