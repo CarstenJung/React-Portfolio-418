f7d66085114a09fcaee657f69381ce66e1021803
Carsten Jung <39696700+CarstenJung@users.noreply.github.com>
2023-07-05 13:55:52 +0200

    ...

--- Portfolio/src/components/DataSlider.jsx
+++ Portfolio/src/components/DataSlider.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
 
 const DataSlider = ({
   currentItem,
@@ -11,32 +12,47 @@ const DataSlider = ({
 
   useEffect(() => {
     let intersectingCount = 0;
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            intersectingCount++;
-            const id = entry.target.getAttribute("data-id");
-            const item = dataItem.find((item) => item.id === Number(id));
-            if (item) {
-              setTextInView(false);
-              setTimeout(() => {
-                setCurrentItem(item);
-                setTextInView(true);
-              }, 0);
-            }
-          } else {
-            intersectingCount = Math.max(0, intersectingCount - 1);
-            if (intersectingCount === 0) {
-              setTextInView(false);
-            }
-          }
-        });
-      },
-      {
-        rootMargin: "-60% 0px -40% 0px",
+
+    if (inViewRef.current) {
+      for (let child of inViewRef.current.children) {
+        // Set initial opacity to 0 for each child through GSAP
+        gsap.set(child, { opacity: 0 });
       }
-    );
+    }
+
+ const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("data-id");
+      const item = dataItem.find((item) => item.id === Number(id));
+
+      if (entry.isIntersecting) {
+        intersectingCount++;
+
+        if (item) {
+          setTextInView(false);
+          setTimeout(() => {
+            setCurrentItem(item);
+            setTextInView(true);
+          }, 0);
+        }
+
+        // Animate opacity to 1 when the element is intersecting
+        //gsap.to(entry.target, { opacity: 1, duration: 2 });
+        gsap.fromTo(entry.target, { opacity: 0}, { opacity: 1, duration: 2 });
+
+      } else if (!entry.isIntersecting) {
+        intersectingCount = Math.max(0, intersectingCount - 1);
+        if (intersectingCount === 0 && item) {
+          setTextInView(false);
+        }
+      }
+    });
+  },
+  {
+    rootMargin: "-50% 0px -50% 0px",
+  }
+);
 
 
     if (inViewRef.current) {
@@ -54,13 +70,14 @@ const DataSlider = ({
     };
   }, [currentItem]);
 
+
+
   return (
     <div className="dataSlider section">
       <div className="dataSliderWrapper">
         <div className="dataSliderLeft">
-          <div
-            className={`dataTextContainer ${textInView ? "visible" : "hidden"}`} key={currentItem.id}
-          >
+          {textInView && (
+          <div className="dataTextContainer" key={currentItem.id}>
             <h1>{currentItem.title}</h1>
             <h3>{currentItem.subtitle}</h3>
             <p>{currentItem.description}</p>
@@ -69,7 +86,9 @@ const DataSlider = ({
               {btnText}
             </a>
           </div>
+          )}
         </div>
+
         <div className="dataSliderRight" ref={inViewRef} >
           {dataItem.map((item) => (
             <div key={item.id} data-id={item.id} className="slideImgWrapper">
