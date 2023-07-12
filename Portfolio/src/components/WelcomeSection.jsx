bcb5ff04c996035862c2919c6c33ff861432f9db
Carsten <39696700+CarstenProjects@users.noreply.github.com>
2023-07-12 22:09:55 +0200

    ...

--- Portfolio/src/components/WelcomeSection.jsx
+++ Portfolio/src/components/WelcomeSection.jsx
@@ -9,9 +9,10 @@ const WelcomeSection = () => {
   const underlineRef = useRef();
 
   useEffect(() => {
+    if (window.innerWidth > 1366) {
     const split = new SplitText(textRef.current, { type: "words,chars" });
-    const chars = split.chars; //an array of all the divs that wrap each character
-    gsap.set(textRef.current, { visibility: "visible" });
+    const chars = split.chars;
+
     gsap.from(chars, {
       opacity: 0,
       y: 10,
@@ -21,11 +22,14 @@ const WelcomeSection = () => {
     });
 
     gsap.fromTo(underlineRef.current, {opacity: 0, y: 20}, {opacity: 1, y:0, duration: .7, delay: .8});
+  } else {
+    gsap.fromTo(textRef.current, {opacity: 0, y: 20}, {opacity: 1, y:0, duration: .7, delay: .8});
+  }
   }, []);
 
   return (
     <div className="welcomeSection section" id="home">
-      <h1 ref={textRef} style={{ visibility: "hidden" }}>
+      <h1 ref={textRef}>
         Carsten Jung
       </h1>
       <p ref={underlineRef}>Self-Taught Frontend Web Developer</p>
