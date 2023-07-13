9544306de9d7fa8847de65191cda1aa08f7139b3
Carsten <39696700+CarstenProjects@users.noreply.github.com>
2023-07-13 22:23:42 +0200

    ...

--- Portfolio/src/assets/data/DataItems.jsx
+++ Portfolio/src/assets/data/DataItems.jsx
@@ -1,19 +1,19 @@
 // Image import
-import professionalCertificateCS50 from '../img/certificates/cs50-professional.jpg';
-import CS50X from '../img/certificates/CS50x.jpg'
-import CS50W from '../img/certificates/CS50w.jpg'
-import ADVANCEDREACT from '../img/certificates/advancedReact.jpg'
-import JAVASCRIPT from '../img/certificates/javascript.jpg'
-import ADOBEXD from '../img/certificates/adobeXD.jpg'
-import HTMLCSS from '../img/certificates/html-css.jpg'
+import professionalCertificateCS50 from "../img/certificates/cs50-professional.jpg";
+import CS50X from "../img/certificates/CS50x.jpg";
+import CS50W from "../img/certificates/CS50w.jpg";
+import ADVANCEDREACT from "../img/certificates/advancedReact.jpg";
+import JAVASCRIPT from "../img/certificates/javascript.jpg";
+import ADOBEXD from "../img/certificates/adobeXD.jpg";
+import HTMLCSS from "../img/certificates/html-css.jpg";
 
-import KREATIFABRIK from '../img/websites/KreatiFabrik.jpg'
-import PETERSEN from '../img/websites/Petersen.jpg'
-import KIENINGER from '../img/websites/Kieninger.jpg'
-import SNAPERS from '../img/websites/Snapers.jpg'
+import KREATIFABRIK from "../img/websites/KreatiFabrik.jpg";
+import PETERSEN from "../img/websites/Petersen.jpg";
+import KIENINGER from "../img/websites/Kieninger.jpg";
+import SNAPERS from "../img/websites/Snapers.jpg";
 
 export const DataItems = [
-    {
+  {
     id: 1,
     title: "HarvardX",
     subtitle: "Professional Certificate • Computer Science for Web Programming",
@@ -21,17 +21,18 @@ export const DataItems = [
     subdescription: "Graded Course • 2 Semester",
     link: "https://credentials.edx.org/credentials/3210ce4eff644a2b8839ef5be07ea334/",
     image: professionalCertificateCS50,
-    btnText: "View Certificate"
+    btnText: "View Certificate",
   },
   {
     id: 2,
     title: "HarvardX",
-    subtitle: "Verified Certificate • CS50's Web Programming with Python and JavaScript",
+    subtitle:
+      "Verified Certificate • CS50's Web Programming with Python and JavaScript",
     description: "HarvardX - Verified Certificate",
     subdescription: "Graded Course • 1 Semester",
     link: "https://courses.edx.org/certificates/752a5ca2d8274aa9bb59a410b7d3fe73",
     image: CS50W,
-    btnText: "View Certificate"
+    btnText: "View Certificate",
   },
   {
     id: 3,
@@ -41,17 +42,18 @@ export const DataItems = [
     subdescription: "Graded Course • 1 Semester",
     link: "https://courses.edx.org/certificates/1ed007a7e68c4a11b5a686285850ec01",
     image: CS50X,
-    btnText: "View Certificate"
+    btnText: "View Certificate",
   },
   {
     id: 4,
     title: "Meta",
-    subtitle: "Authorized by Meta and offered through Coursera • Advanced React",
+    subtitle:
+      "Authorized by Meta and offered through Coursera • Advanced React",
     description: "Meta - Course Certificate",
     subdescription: "Graded Course • Non-Credit",
     link: "https://www.coursera.org/account/accomplishments/verify/PRLYME3J48UX",
     image: ADVANCEDREACT,
-    btnText: "View Certificate"
+    btnText: "View Certificate",
   },
   {
     id: 5,
@@ -61,7 +63,7 @@ export const DataItems = [
     subdescription: "Online course • 11 hours",
     link: "ude.my/UC-Z484ZM",
     image: JAVASCRIPT,
-    btnText: "View Certificate"
+    btnText: "View Certificate",
   },
   {
     id: 6,
@@ -71,17 +73,18 @@ export const DataItems = [
     subdescription: "Online course • 11.5 hours",
     link: "ude.my/UC-MOYLTKZ",
     image: ADOBEXD,
-    btnText: "View Certificate"
+    btnText: "View Certificate",
   },
   {
     id: 7,
     title: "Udemy",
-    subtitle: "Online HTML/CSS • Build Websites with HTML5 & CSS3: Beginner to Pro!",
+    subtitle:
+      "Online HTML/CSS • Build Websites with HTML5 & CSS3: Beginner to Pro!",
     description: "Udemy - Certificate of Completion",
     subdescription: "Online course • 16 hours",
     link: "ude.my/UC-E7SY10",
     image: HTMLCSS,
-    btnText: "View Certificate"
+    btnText: "View Certificate",
   },
   {
     id: 8,
@@ -91,27 +94,29 @@ export const DataItems = [
     subdescription: "",
     link: "",
     image: "",
-    btnText: "View CV"
+    btnText: "View CV",
   },
   {
     id: 9,
     title: "Kreatifabrik",
     subtitle: "Website for a marketing and advertising agency",
-    description: "WordPress website created with DIVI, HTML, CSS and Java Script",
+    description:
+      "WordPress website created with DIVI, HTML, CSS and Java Script",
     subdescription: "Effects with GSAP • Green Sock Animation",
     link: "https://kreatifabrik.de/",
     image: KREATIFABRIK,
-    btnText: "Visit Website"
+    btnText: "Visit Website",
   },
   {
     id: 10,
     title: "Petersen",
     subtitle: "Company page of a craft market",
-    description: "WordPress website created with DIVI, HTML, CSS and Java Script",
+    description:
+      "WordPress website created with DIVI, HTML, CSS and Java Script",
     subdescription: "Effects with GSAP • Green Sock Animation",
     link: "https://petersen-vs.de/",
     image: PETERSEN,
-    btnText: "Visit Website"
+    btnText: "Visit Website",
   },
   {
     id: 11,
@@ -121,16 +126,17 @@ export const DataItems = [
     subdescription: "Adjusted with Liquid • Green Sock Animation",
     link: "https://www.snapers.de/",
     image: SNAPERS,
-    btnText: "Visit Shop"
+    btnText: "Visit Shop",
   },
   {
     id: 12,
     title: "Kieninger",
     subtitle: "Company page of a consulting agency",
-    description: "WordPress website created with DIVI, HTML, CSS and Java Script",
+    description:
+      "WordPress website created with DIVI, HTML, CSS and Java Script",
     subdescription: "Effects with GSAP • Green Sock Animation",
     link: "https://kieninger-consulting.ch/",
     image: KIENINGER,
-    btnText: "Visit Website"
-  }
-]
+    btnText: "Visit Website",
+  },
+];
