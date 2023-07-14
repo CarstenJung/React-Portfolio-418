1c8b1d3fa5bf655ebf0f4d68ef137a562eb9e1f2
Carsten <39696700+CarstenProjects@users.noreply.github.com>
2023-07-14 16:21:15 +0200

    ...

--- Portfolio/src/components/ModalForm.jsx
+++ Portfolio/src/components/ModalForm.jsx
@@ -1,58 +1,81 @@
+import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import {  Modal } from "@mui/material";
-
+import axios from "axios";
+import { Modal } from "@mui/material";
 
 const ModalForm = ({ open, handleClose }) => {
   const validationSchema = Yup.object().shape({
     name: Yup.string().required("Name is required"),
-    email: Yup.string()
-      .email("Invalid email address")
-      .required("Email is required"),
+    email: Yup.string().email("Invalid email address").required("Email is required"),
     message: Yup.string().required("Message is required"),
   });
 
+  const submitForm = async (values, { setSubmitting }) => {
+    try {
+      await axios.post("/", values, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+      console.log("Form submitted successfully");
+      setSubmitting(false);
+      handleClose();
+    } catch (error) {
+      console.error("Error submitting form", error);
+      setSubmitting(false);
+    }
+  };
+
   return (
     <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-      >
+      <Modal open={open} onClose={handleClose}>
         <div className="contactFormWrapper">
-          <button className="closeModal" onClick={handleClose}>x</button>
+          <button className="closeModal" onClick={handleClose}>
+            x
+          </button>
           <h1 id="modal-title">Get in Touch</h1>
           <p>In Search of My 'TypeError: Company Not Found'</p>
           <Formik
             initialValues={{ name: "", company: "", email: "", message: "" }}
             validationSchema={validationSchema}
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                /* alert(JSON.stringify(values, null, 2)); */
-                setSubmitting(false);
-                handleClose();
-              }, 400);
-            }}
+            onSubmit={submitForm}
           >
-            <Form method="POST" className="contactFormModal" data-netlify="true" name="contact-form">
-            <input type="hidden" name="form-name" value="contact-form" />
-              <label htmlFor="name">Name</label>
-              <Field name="name" type="text" id="name" />
-              <ErrorMessage name="name" component="div" />
-              
-              <label htmlFor="company">Company</label>
-              <Field name="company" type="text" id="company" />
-              <ErrorMessage name="company" component="div" />
+            {({ isSubmitting }) => (
+              <Form
+                name="contact-form"
+                method="post"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+              >
+                <input type="hidden" name="form-name" value="contact-form" />
+                <div hidden>
+                  <label>
+                    Don’t fill this out if you're human: <input name="bot-field" />
+                  </label>
+                </div>
 
-              <label htmlFor="email">Email</label>
-              <Field name="email" type="email" id="email" />
-              <ErrorMessage name="email" component="div" />
+                <label htmlFor="name">Name</label>
+                <Field name="name" type="text" id="name" />
+                <ErrorMessage name="name" component="div" />
 
-              <label htmlFor="message">Message</label>
-              <Field name="message" component="textarea" id="message" />
-              <ErrorMessage name="message" component="div" />
+                <label htmlFor="company">Company</label>
+                <Field name="company" type="text" id="company" />
+                <ErrorMessage name="company" component="div" />
 
-              <button type="submit">Submit</button>
-            </Form>
+                <label htmlFor="email">Email</label>
+                <Field name="email" type="email" id="email" />
+                <ErrorMessage name="email" component="div" />
+
+                <label htmlFor="message">Message</label>
+                <Field name="message" component="textarea" id="message" />
+                <ErrorMessage name="message" component="div" />
+
+                <button type="submit" disabled={isSubmitting}>
+                  {isSubmitting ? "Submitting" : "Submit"}
+                </button>
+              </Form>
+            )}
           </Formik>
         </div>
       </Modal>
